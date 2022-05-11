c1a3a0a0897ac8a2f5e921048bdecc089f0c91f3
Viktor Hultman <viktor.hultman@inosida.se>
2022-05-11 14:59:27 +0200

    Update to gitignore and readme

--- src/Chat.svelte
+++ src/Chat.svelte
@@ -81,7 +81,13 @@ import { afterUpdate } from 'svelte';
         }
     }
 
-    const openAChannel = async (channel_id) => {
+    const openAChannel = async (event, channel_id) => {
+        if(channel_id == $currentChannel.id){
+            return console.log("Channel already open");
+        }
+        if(event.target.nodeName !== "DIV"){
+            return console.log("You clicked something else than the channel div");
+        }
         //Setting the id of the opened channel to local storage
         localStorage.setItem('lastOpenedChannel', channel_id)
         openChannel(channel_id)
@@ -205,7 +211,7 @@ import { afterUpdate } from 'svelte';
                 {#if $channels != null}
                     {#each $channels as channel (channel.id)}
                         {#if dbUser && (dbUser.hidden_channels == null || !dbUser.hidden_channels.includes(channel.id))}
-                            <div class="channel" on:click={() => openAChannel(channel.id)}>
+                            <div class="channel" on:click={(e) => openAChannel(e, channel.id)}>
                                 <div class="channel-info">
                                     <div class="channel-icon"></div>
                                     <p>{truncateString(channel.channel_name, 29)}</p>
@@ -249,9 +255,9 @@ import { afterUpdate } from 'svelte';
                 <div class="curC-nav">
                     <div class="curC-info">
                         <h3 class="curC-name">{$currentChannel.channel_name}</h3>
-                        <div class="setting-icon">
+                        <!-- <div class="setting-icon">
                             <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" viewBox="0 0 1792 1792"><path d="M1152 896q0-106-75-181t-181-75-181 75-75 181 75 181 181 75 181-75 75-181zm512-109v222q0 12-8 23t-20 13l-185 28q-19 54-39 91 35 50 107 138 10 12 10 25t-9 23q-27 37-99 108t-94 71q-12 0-26-9l-138-108q-44 23-91 38-16 136-29 186-7 28-36 28h-222q-14 0-24.5-8.5t-11.5-21.5l-28-184q-49-16-90-37l-141 107q-10 9-25 9-14 0-25-11-126-114-165-168-7-10-7-23 0-12 8-23 15-21 51-66.5t54-70.5q-27-50-41-99l-183-27q-13-2-21-12.5t-8-23.5v-222q0-12 8-23t19-13l186-28q14-46 39-92-40-57-107-138-10-12-10-24 0-10 9-23 26-36 98.5-107.5t94.5-71.5q13 0 26 10l138 107q44-23 91-38 16-136 29-186 7-28 36-28h222q14 0 24.5 8.5t11.5 21.5l28 184q49 16 90 37l142-107q9-9 24-9 13 0 25 10 129 119 165 170 7 8 7 22 0 12-8 23-15 21-51 66.5t-54 70.5q26 50 41 98l183 28q13 2 21 12.5t8 23.5z"/></svg>
-                        </div>
+                        </div> -->
                         <div class="curC-users">
                             <div class="user-avatars">
                                 {#each $allUsers as user (user.id)}
@@ -426,14 +432,14 @@ import { afterUpdate } from 'svelte';
 
     .channels-container {
         height:calc(100% - 150px);
-        overflow-y: scroll;
+        overflow-y: auto;
         color: white;
     }
 
     .channel {
         height: 50px;
         margin: 5px 0px 5px 0px;
-        padding-left: 20px;
+        padding: 0px 10px 0px 20px;
         display: flex;
         align-items: center;
         justify-content: space-between;
@@ -466,9 +472,11 @@ import { afterUpdate } from 'svelte';
     }
 
     .setting-icon {
+        display: flex;
+        align-items: center;
+        justify-content: center;
         min-width: 20px;
         height: 20px;
-        padding: 10px;
         color: inherit;
     }
     .setting-icon:hover {
@@ -478,6 +486,9 @@ import { afterUpdate } from 'svelte';
     .setting-icon:active {
         color: rgba(255, 255, 255, 0.8);
         cursor: pointer;
+    }
+    .setting-icon svg {
+        padding: 10px;
     }
 
     .profile-container {
@@ -551,7 +562,7 @@ import { afterUpdate } from 'svelte';
         border: 1px solid #BBBBBB;
         border-radius: 5px;
         padding: 4px;
-        margin-left: 10px;
+        margin-left: 20px;
     }
 
     .curC-users:hover {
@@ -593,7 +604,7 @@ import { afterUpdate } from 'svelte';
     .chat {
         height:calc(100% - 150px);
         max-width: 100%;
-        overflow-y: scroll;
+        overflow-y: auto;
         overflow-x: hidden;
         padding: 20px 0px;
     }
