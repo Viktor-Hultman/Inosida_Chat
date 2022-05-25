5dc9d9d2dfb1b45d1585ad4e40e141482b4cc492
Viktor Hultman <viktor.hultman@inosida.se>
2022-05-25 10:45:26 +0200

    Added some styling for max width for icons

--- src/Chat.svelte
+++ src/Chat.svelte
@@ -470,7 +470,7 @@ import { afterUpdate } from 'svelte';
                 <div class="chat-user">
                     <img class="chat-avatar" src="{user.userdata.avatar_url}" alt="channel user avatar">
                     <p>{truncateString(user.userdata.full_name, 20)}</p>
-                    <div class="plus-icon" tabindex="0" on:keypress={(e) => e.keyCode === 13 && addAUserToChannel($currentChannel.id, user.id)} on:click={(e) => addAUserToChannel($currentChannel.id, user.id)}>
+                    <div class="plus-icon" tabindex="0" on:keypress={(e) => e.keyCode === 13 && addAUserToChannel($currentChannel.id, user.id)} on:click={() => addAUserToChannel($currentChannel.id, user.id)}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" viewBox="0 0 1792 1792"><path d="M1600 736v192q0 40-28 68t-68 28h-416v416q0 40-28 68t-68 28h-192q-40 0-68-28t-28-68v-416h-416q-40 0-68-28t-28-68v-192q0-40 28-68t68-28h416v-416q0-40 28-68t68-28h192q40 0 68 28t28 68v416h416q40 0 68 28t28 68z"/></svg>
                     </div>
                 </div>
@@ -496,7 +496,7 @@ import { afterUpdate } from 'svelte';
                 <div class="chat-user">
                     <img class="chat-avatar" src="{user.userdata.avatar_url}" alt="channel user avatar">
                     <p>{truncateString(user.userdata.full_name, 20)}</p>
-                    <div class="plus-icon" tabindex="0" on:keypress={(e) => e.keyCode === 13 && removeAUserfromChannel($currentChannel.id, user.id)} on:click={(e) => removeAUserfromChannel($currentChannel.id, user.id)}>
+                    <div class="plus-icon" tabindex="0" on:keypress={(e) => e.keyCode === 13 && removeAUserfromChannel($currentChannel.id, user.id)} on:click={() => removeAUserfromChannel($currentChannel.id, user.id)}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" viewBox="0 0 1792 1792"><path d="M1600 736v192q0 40-28 68t-68 28h-1216q-40 0-68-28t-28-68v-192q0-40 28-68t68-28h1216q40 0 68 28t28 68z"/></svg>
                     </div>
                 </div>
