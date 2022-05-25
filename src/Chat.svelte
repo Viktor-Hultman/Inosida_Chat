e6e88df1e4c200b65f5b5cba3788c23c0999a21a
Viktor Hultman <viktor.hultman@inosida.se>
2022-05-25 11:09:01 +0200

    Added toggle to another element

--- src/Chat.svelte
+++ src/Chat.svelte
@@ -546,7 +546,7 @@ import { afterUpdate } from 'svelte';
             <div bind:this={sidebar} class="sidebar">
                 <div class="add-chat-section">
                     <h3>Chattrum</h3>
-                    <div tabindex="0" class="plus-icon" on:keypress={(e) => e.keyCode === 13 && createNewChannel()} on:click={() => createNewChannel()}>
+                    <div tabindex="0" class="plus-icon" on:keypress={(e) => e.keyCode === 13 && (createNewChannel(), toggleSidebar())} on:click={(() => createNewChannel(), toggleSidebar())}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" viewBox="0 0 1792 1792"><path d="M1600 736v192q0 40-28 68t-68 28h-416v416q0 40-28 68t-68 28h-192q-40 0-68-28t-28-68v-416h-416q-40 0-68-28t-28-68v-192q0-40 28-68t68-28h416v-416q0-40 28-68t68-28h192q40 0 68 28t28 68v416h416q40 0 68 28t28 68z"/></svg>
                     </div>
                 </div>
